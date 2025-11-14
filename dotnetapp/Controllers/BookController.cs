803d4c29d22181d905aef323faf4b3e9053fe9f0
Ayush0826 <ayushagarwal0826@gmail.com>
2025-11-14 10:29:56 +0000

    updated backend files controller username -AA

--- dotnetapp/Controllers/BookController.cs
+++ dotnetapp/Controllers/BookController.cs
@@ -52,7 +52,6 @@ namespace dotnetapp.Controllers
 
         [HttpPost]
         [Authorize(Roles = "BookRecommender")]
-        //  [AllowAnonymous]
         public async Task<ActionResult> AddBook([FromBody] Book book)
         {
             try
